daec746eff9b161bea5cd95e59b52607480b57bb
Yahsai <yahsaisantana1@gmail.com>
2024-06-21 13:02:43 -0400

    update server

--- public/src/server.js
+++ public/src/server.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
-const db = require('./database/connection');
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
 const usersRoutes = require('./routes/users');
 const gameStateRoutes = require('./routes/gamestate');
 const scoresRoutes = require('./routes/scores');
@@ -13,13 +9,34 @@ const scoresRoutes = require('./routes/scores');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
-app.use(express.static('../public'));
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Study and Fun API',
+      version: '1.0.0',
+      description: 'API for Study and Fun application'
+    },
+    servers: [
+      { url: 'http://localhost:3000' }
+    ]
+  },
+  apis: ['./routes/*.js']
+};
 
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+app.use(bodyParser.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use('/api/users', usersRoutes);
 app.use('/api/gamestate', gameStateRoutes);
 app.use('/api/scores', scoresRoutes);
 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Something went wrong!' });
+});
+
 app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
